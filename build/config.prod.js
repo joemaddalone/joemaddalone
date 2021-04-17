1dcf372bb2445890744b76caf5a14efa01839548
Joe Maddalone <>
2021-04-17 10:42:31 -0500

    esbuild minify prod

--- build/config.prod.js
+++ build/config.prod.js
@@ -1,6 +1,7 @@
 const config = require("./config.common");
 
 module.exports = Object.assign(config, {
+	minify: true,
   define: Object.assign(config.define, {
     "process.env.NODE_ENV": `"productions"`,
   })
