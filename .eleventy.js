b6b57574759147ead5ce3938470ed66ef7040b0e
joemaddalone <joe.maddalone@gmail.com>
2021-11-29 20:18:52 -0600

    more progress

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,16 @@
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPlugin(syntaxHighlight);
+	eleventyConfig.addFilter('readableDate', (dateObj) => {
+		return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('LLL dd yyyy');
+	});
+
+	// https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+	eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+		return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
+	});
 	return {
 		dir: {
 			input: 'src/writes',
