8c724c593eed8e4f28dab9f13338dfb54576afaf
Joe Maddalone <joe.maddalone@gmail.com>
2025-08-06 10:31:25 -0500

    code style

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,14 @@ import mdx from '@astrojs/mdx';
 
 import react from '@astrojs/react';
 
+import expressiveCode from 'astro-expressive-code';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()],
+  integrations: [expressiveCode({
+    themes: ['snazzy-light'],
+  }), mdx(), react()],
+
   markdown: {
     shikiConfig: {
       theme: 'snazzy-light',
